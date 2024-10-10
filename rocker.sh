c15198a797737c13a043b5a60381e0e88a7175d9
Eli Holmes - NOAA <eli.holmes@noaa.gov>
2024-10-10 12:56:51 -0700

    Update rocker.sh

--- rocker.sh
+++ rocker.sh
@@ -7,7 +7,7 @@ set -e
 
 R_VERSION="4.4.1"
 
-# Copy in the rocker files
+# Copy in the rocker files. Work in ${REPO_DIR} to make sure I don't clobber anything
 cd ${REPO_DIR}
 wget https://github.com/rocker-org/rocker-versioned2/archive/refs/tags/R${R_VERSION}.tar.gz
 tar zxvf R${R_VERSION}.tar.gz && \
@@ -16,6 +16,7 @@ mv rocker-versioned2-R${R_VERSION}/dockerfiles/verse_${R_VERSION}.Dockerfile /ro
 mv rocker-versioned2-R${R_VERSION}/dockerfiles/r-ver_${R_VERSION}.Dockerfile /rocker_scripts/r-ver_${R_VERSION}.Dockerfile && \
 rm R${R_VERSION}.tar.gz && \
 rm -rf rocker-versioned2-R${R_VERSION}
+cd /
 
 # Read the Dockerfile and process each line
 while IFS= read -r line; do
