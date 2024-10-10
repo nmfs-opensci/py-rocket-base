6c6f8a32ddb581ea611be4565df1b578e78a81f0
Eli Holmes - NOAA <eli.holmes@noaa.gov>
2024-10-10 14:45:36 -0700

    Update rocker.sh

--- rocker.sh
+++ rocker.sh
@@ -4,8 +4,7 @@ set -e
 # This script will copy in the rocker_scripts to install things and
 # Install rocker-verse using the verse_${R_VERSION}.Dockerfile file
 # It will run just the ENV and RUN commands in that file
-
-R_VERSION="4.4.1"
+# Variables defined here will only be available in this script.
 
 # Copy in the rocker files. Work in ${REPO_DIR} to make sure I don't clobber anything
 cd ${REPO_DIR}
@@ -16,14 +15,16 @@ mv rocker-versioned2-R${R_VERSION}/dockerfiles/verse_${R_VERSION}.Dockerfile /ro
 mv rocker-versioned2-R${R_VERSION}/dockerfiles/r-ver_${R_VERSION}.Dockerfile /rocker_scripts/r-ver_${R_VERSION}.Dockerfile && \
 rm R${R_VERSION}.tar.gz && \
 rm -rf rocker-versioned2-R${R_VERSION}
-cd /
 
+cd /
 # Read the Dockerfile and process each line
 while IFS= read -r line; do
     # Check if the line starts with ENV or RUN
     if [[ "$line" == ENV* ]]; then
-        # Export the environment variable from the ENV line
-        eval $(echo "$line" | sed 's/^ENV //g')
+        # Extract the variable assignment
+        var_assignment=$(echo "$line" | sed 's/^ENV //g')        
+        # Export the environment variable
+        export "$var_assignment"
     elif [[ "$line" == RUN* ]]; then
         # Run the command from the RUN line
         cmd=$(echo "$line" | sed 's/^RUN //g')
