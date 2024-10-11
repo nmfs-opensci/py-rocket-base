572b58c8bfac80e956aa5830f4a151750bda76ac
Eli Holmes - NOAA <eli.holmes@noaa.gov>
2024-10-10 17:49:20 -0700

    Update rocker.sh

--- rocker.sh
+++ rocker.sh
@@ -8,7 +8,8 @@ set -e
 
 # Copy in the rocker files. Work in ${REPO_DIR} to make sure I don't clobber anything
 cd ${REPO_DIR}
-wget https://github.com/rocker-org/rocker-versioned2/archive/refs/tags/R${R_VERSION}.tar.gz
+wget https://github.com/eeholmes/rocker-versioned2/archive/refs/heads/master.tar.gz
+#wget https://github.com/rocker-org/rocker-versioned2/archive/refs/tags/R${R_VERSION}.tar.gz
 tar zxvf R${R_VERSION}.tar.gz && \
 mv rocker-versioned2-R${R_VERSION}/scripts /rocker_scripts && \
 mv rocker-versioned2-R${R_VERSION}/dockerfiles/verse_${R_VERSION}.Dockerfile /rocker_scripts/verse_${R_VERSION}.Dockerfile && \
