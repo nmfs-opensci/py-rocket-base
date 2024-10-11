9404ed0babc2d706dc75762e3d829a1b0d9c07c3
Eli Holmes - NOAA <eli.holmes@noaa.gov>
2024-10-11 11:34:34 -0700

    Update rocker.sh

--- rocker.sh
+++ rocker.sh
@@ -12,7 +12,7 @@ cd ${REPO_DIR}
 wget https://github.com/rocker-org/rocker-versioned2/archive/refs/tags/R${R_VERSION}.tar.gz
 tar zxvf R${R_VERSION}.tar.gz && \
 mv rocker-versioned2-R${R_VERSION}/scripts /rocker_scripts && \
-mv rocker-versioned2-R${R_VERSION}/dockerfiles/${R_TAG}_${R_VERSION}.Dockerfile /rocker_scripts/original.Dockerfile && \
+mv rocker-versioned2-R${R_VERSION}/dockerfiles/${R_DOCKERFILE}_${R_VERSION}.Dockerfile /rocker_scripts/original.Dockerfile && \
 rm R${R_VERSION}.tar.gz && \
 rm -rf rocker-versioned2-R${R_VERSION}
 
