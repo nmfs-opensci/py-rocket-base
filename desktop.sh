cf7c52ce3fdd6af06243168a7031f524061c1674
Eli Holmes - NOAA <eli.holmes@noaa.gov>
2024-10-30 13:41:27 -0700

    Update desktop.sh

--- desktop.sh
+++ desktop.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# The default for XDG and xfce4 is for Desktop files to be in ~/Desktop but this leads to a variety of problems
+# First we are altering the user directiory which seems rude, second orphan desktop files might be in ~/Desktop so who knows
+# what the user Desktop experience with be, here the Desktop dir is set to /usr/share/Desktop so is part of the image.
+# users that really want to customize Desktop can change ~/.config/user-dirs.dirs. Though py-rocket-base might not respect that.
 set -e
 
 # set the Desktop dir to something in base
@@ -8,6 +12,8 @@ echo 'XDG_DESKTOP_DIR="/usr/share/Desktop"' > /etc/xdg/user-dirs.defaults
 APPLICATIONS_DIR=/usr/share/applications
 DESKTOP_DIR=/usr/share/Desktop
 mkdir -p "${DESKTOP_DIR}"
+chown :staff /usr/share/Desktop
+chmod 775 /usr/share/Desktop
 # set the Desktop dir default for XDG
 echo 'XDG_DESKTOP_DIR="${DESKTOP_DIR}"' > /etc/xdg/user-dirs.defaults
 
