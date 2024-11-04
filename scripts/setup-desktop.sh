8836e924e5db8e82031db87ef330308733929713
Eli Holmes - NOAA <eli.holmes@noaa.gov>
2024-11-04 08:27:32 -0800

    Update setup-desktop.sh

--- scripts/setup-desktop.sh
+++ scripts/setup-desktop.sh
@@ -10,9 +10,6 @@ fi
 # The rest of the script continues here, running as root
 echo "Running setup-desktop.sh as root. Proceeding with installation..."
 
-# Ensure files are copied before proceeding
-/scripts/copy-files.sh
-
 echo "Checking for Desktop directory..."
 cd "${REPO_DIR}/childimage/" || exit 1
 if test -d Desktop; then
