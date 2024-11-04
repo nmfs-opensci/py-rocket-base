19ae44c98882ce0ae11e8093cf9634eafec043bc
Eli Holmes - NOAA <eli.holmes@noaa.gov>
2024-11-04 09:02:50 -0800

    Update install-conda-packages.sh

--- scripts/install-conda-packages.sh
+++ scripts/install-conda-packages.sh
@@ -3,25 +3,25 @@
 
 echo "Running install-conda-packages.sh"
 
-echo "Checking for ${REPO_DIR}/childimage/..."
+echo "  Checking for ${REPO_DIR}/childimage/..."
 if [ -d "${REPO_DIR}/childimage/" ]; then
     cd "${REPO_DIR}/childimage/" || exit 1
 
-    echo "Checking for conda-lock.yml or environment.yml in ${REPO_DIR}/childimage/..."
+    echo "  Checking for conda-lock.yml or environment.yml in ${REPO_DIR}/childimage/..."
     if test -f "conda-lock.yml" || test -f "environment.yml"; then
         # Switch to NB_USER only if the relevant files exist
         if [[ $(id -u) -eq 0 ]]; then
-            echo "Switching to ${NB_USER} to run install-conda-packages.sh"
+            echo "  Switching to ${NB_USER} to run install-conda-packages.sh"
             exec su "${NB_USER}" -c "/bin/bash $0"  # Switches to NB_USER and reruns the script
         fi
         
         if test -f "conda-lock.yml"; then
-            echo "Using conda-lock.yml"
+            echo "  Using conda-lock.yml"
             conda-lock install --name ${CONDA_ENV}
             pip install --no-deps jupyter-remote-desktop-proxy
             INSTALLATION_HAPPENED=true
         elif test -f "environment.yml"; then
-            echo "Using environment.yml"
+            echo "  Using environment.yml"
             mamba env update --name ${CONDA_ENV} -f environment.yml
             pip install --no-deps jupyter-remote-desktop-proxy
             INSTALLATION_HAPPENED=true
@@ -37,8 +37,8 @@ if [ -d "${REPO_DIR}/childimage/" ]; then
             fi
         fi
     else
-        echo "No conda-lock.yml or environment.yml found. Skipping installation."
+        echo "  No conda-lock.yml or environment.yml found. Skipping installation."
     fi
 else
-    echo "Directory ${REPO_DIR}/childimage/ does not exist. Skipping script."
+    echo "  Directory ${REPO_DIR}/childimage/ does not exist. Skipping script."
 fi
