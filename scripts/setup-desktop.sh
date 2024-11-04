99d9c5933157b37e1cb31b8d455954120ef1ddb4
Eli Holmes - NOAA <eli.holmes@noaa.gov>
2024-11-04 08:06:28 -0800

    Update setup-desktop.sh

--- scripts/setup-desktop.sh
+++ scripts/setup-desktop.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+# Required User: root
+
+# Check if the script is run as root
+if [[ $(id -u) -ne 0 ]]; then
+    echo "Error: This script must be run as root." >&2  # Output error message to standard error
+    exit 1  # Exit with a non-zero status to indicate failure
+fi
+
+# The rest of the script continues here, running as root
+echo "Running setup-desktop.sh as root. Proceeding with installation..."
+
 # Ensure files are copied before proceeding
 /scripts/copy-files.sh
 
