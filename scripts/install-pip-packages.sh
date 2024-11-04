fefbd012b3d5950529f802f7eb257884fc734491
Eli Holmes - NOAA <eli.holmes@noaa.gov>
2024-11-04 09:03:09 -0800

    Update install-pip-packages.sh

--- scripts/install-pip-packages.sh
+++ scripts/install-pip-packages.sh
@@ -3,11 +3,11 @@
 
 echo "Running install-pip-packages.sh"
 
-echo "Checking for ${REPO_DIR}/childimage/..."
+echo "  Checking for ${REPO_DIR}/childimage/..."
 if [ -d "${REPO_DIR}/childimage/" ]; then
     cd "${REPO_DIR}/childimage/" || exit 1
 
-    echo "Checking for requirements.txt in ${REPO_DIR}/childimage/..."
+    echo "  Checking for requirements.txt in ${REPO_DIR}/childimage/..."
     if test -f "requirements.txt"; then
         # Switch to NB_USER only if requirements.txt exists
         if [[ $(id -u) -eq 0 ]]; then
@@ -15,11 +15,11 @@ if [ -d "${REPO_DIR}/childimage/" ]; then
             exec su "${NB_USER}" -c "/bin/bash $0"  # Switches to NB_USER and reruns the script
         fi
 
-        echo "Installing pip packages from requirements.txt as ${NB_USER}..."
+        echo "  Installing pip packages from requirements.txt as ${NB_USER}..."
         ${NB_PYTHON_PREFIX}/bin/pip install --no-cache -r requirements.txt
     else
-        echo "No requirements.txt found. Skipping pip installation."
+        echo "  No requirements.txt found. Skipping pip installation."
     fi
 else
-    echo "Directory ${REPO_DIR}/childimage/ does not exist. Skipping script."
+    echo "  Directory ${REPO_DIR}/childimage/ does not exist. Skipping script."
 fi
