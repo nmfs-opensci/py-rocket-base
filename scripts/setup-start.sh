760199a6d15258be1182e25db9bf7e323ffb900d
Eli Holmes - NOAA <eli.holmes@noaa.gov>
2024-11-04 09:11:27 -0800

    Update setup-start.sh

--- scripts/setup-start.sh
+++ scripts/setup-start.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
 # Required User: NB_USER
 
-echo "Checking for ${REPO_DIR}/childimage/..."
+echo "Running setup-start.sh"
+
+echo "  Checking for ${REPO_DIR}/childimage/..."
 if [ -d "${REPO_DIR}/childimage/" ]; then
     cd "${REPO_DIR}/childimage/" || exit 1
 
-    echo "Checking for start..."
+    echo "  Checking for start in ${REPO_DIR}/childimage/..."
     if test -f "start"; then
+        echo "  start found in ${REPO_DIR}/childimage/."
         # Switch to NB_USER only if the start file exists
         if [[ $(id -u) -eq 0 ]]; then
-            echo "Switching to ${NB_USER} to run setup-start.sh"
+            echo "  Switching to ${NB_USER} to run setup-start.sh"
             exec su "${NB_USER}" -c "/bin/bash $0"  # Switches to NB_USER and reruns the script
         fi
-
-        echo "Running setup-start.sh as ${NB_USER}"
         chmod +x start
-        # Optionally, you can execute the start script here
-        ./start
     else
-        echo "No start file found. Skipping execution."
+        echo "  No start file found in ${REPO_DIR}/childimage/. Skipping."
     fi
 else
-    echo "Directory ${REPO_DIR}/childimage/ does not exist. Exiting gracefully."
-    exit 0  # Exit with a success status
+    echo "  Directory ${REPO_DIR}/childimage/ does not exist. Skipping."
 fi
