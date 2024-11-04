2b3552bb1e3fa95f062dc53fdd2a7abe3ce5fd2d
Eli Holmes - NOAA <eli.holmes@noaa.gov>
2024-11-04 08:32:17 -0800

    Update install-apt-packages.sh

--- scripts/install-apt-packages.sh
+++ scripts/install-apt-packages.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 # Required User: root
 
-# Check if the script is run as root
-if [[ $(id -u) -ne 0 ]]; then
-    echo "Error: This script must be run as root." >&2  # Output error message to standard error
-    exit 1  # Exit with a non-zero status to indicate failure
-fi
-
-# The rest of the script continues here, running as root
-echo "Running install-apt-packages.sh as root. Proceeding with installation..."
-
 echo "Checking for apt.txt..."
 cd "${REPO_DIR}/childimage/" || exit 1
+
 if test -f "apt.txt"; then
+    # Check if the script is run as root
+    if [[ $(id -u) -ne 0 ]]; then
+        echo "Error: This script must be run as root." >&2  # Output error message to standard error
+        exit 1  # Exit with a non-zero status to indicate failure
+    fi
+
+    echo "Running install-apt-packages.sh as root. Proceeding with installation..."
+
     package_list=$(grep -v '^\s*#' apt.txt | grep -v '^\s*$' | sed 's/\r//g; s/#.*//; s/^[[:space:]]*//; s/[[:space:]]*$//' | awk '{$1=$1};1')
     apt-get update --fix-missing > /dev/null
     apt-get install --yes --no-install-recommends $package_list
