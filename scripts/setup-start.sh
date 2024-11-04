a97014c2183c5390cfd0e6b6716ca6597b8b1256
Eli Holmes - NOAA <eli.holmes@noaa.gov>
2024-11-04 08:08:06 -0800

    Update setup-start.sh

--- scripts/setup-start.sh
+++ scripts/setup-start.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+# Required User: root
+
+# Check if the script is run as root
+if [[ $(id -u) -ne 0 ]]; then
+    echo "Error: This script must be run as root." >&2  # Output error message to standard error
+    exit 1  # Exit with a non-zero status to indicate failure
+fi
+
+# The rest of the script continues here, running as root
+echo "Running setup-start.sh as root. Proceeding with installation..."
+
 /scripts/copy-files.sh
 
 echo "Checking for start..."
