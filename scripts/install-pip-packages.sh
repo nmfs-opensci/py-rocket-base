7e67a037bb78bb4e254d75b80581fe91241f5e97
Eli Holmes - NOAA <eli.holmes@noaa.gov>
2024-11-05 07:48:16 -0800

    Update install-pip-packages.sh

--- scripts/install-pip-packages.sh
+++ scripts/install-pip-packages.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 # Required User: NB_USER
 
-echo "Running install-pip-packages.sh"
+# Check if running as root and switch to NB_USER if needed
+if [[ $(id -u) -eq 0 ]]; then
+    echo "Switching to ${NB_USER} to run install-pip-packages.sh"
+    exec su "${NB_USER}" -c "/bin/bash $0 $1"  # Pass along the filename argument
+fi
 
-# Check that a file name is provided
+# Main script execution as NB_USER
+echo "Running install-pip-packages.sh as ${NB_USER}"
+
+# Check if a filename argument is provided
 if [ -z "$1" ]; then
-    echo "Error: No file provided. Usage: RUN /pyrocket_scripts/install-pip-packages.sh <requirements.txt>"
+    echo "  Error: This script requires a requirements.txt file." >&2
+    echo "  Usage: RUN /pyrocket_scripts/install-pip-packages.sh <filename.yml>" >&2
     exit 1
 fi
 
 # Set variable for the provided file
 requirements_file="$1"
-echo "Using packages file: ${requirements_file}"
+echo "  Using packages file: ${requirements_file}"
 
 # Check if the specified file exists
 if [ ! -f "${requirements_file}" ]; then
-    echo "Error: File '${requirements_file}' not found. Ensure the file exists and try again."
-    exit 1
-fi
-
-# Check if the script is run as the expected user
-if [[ $(id -u) -ne $(id -u "${NB_USER}") ]]; then
-    echo "Error: This script must be run as ${NB_USER}. Please use USER ${NB_USER} in your Dockerfile before running this script."
+    echo "  Error: File '${requirements_file}' not found. Ensure the file exists and try again."
     exit 1
 fi
 
@@ -29,9 +31,9 @@ echo "  Installing pip packages from ${requirements_file} as ${NB_USER}..."
 
 # Install pip packages and handle errors
 if ! ${NB_PYTHON_PREFIX}/bin/pip install --no-cache -r "${requirements_file}"; then
-    echo "Error: Installation of packages from '${requirements_file}' failed. Please check the package names and try again."
+    echo "  Error: Installation of packages from '${requirements_file}' failed. Please check the package names and try again."
     exit 1
 fi
 
-echo "  Pip packages installed successfully."
+echo "  Success! install-pip-packages.sh"
 
