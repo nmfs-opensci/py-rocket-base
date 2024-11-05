77d0479899a19ed1924584563643225e9699aa52
Eli Holmes - NOAA <eli.holmes@noaa.gov>
2024-11-05 07:59:12 -0800

    Update install-desktop.sh

--- scripts/install-desktop.sh
+++ scripts/install-desktop.sh
@@ -3,19 +3,26 @@
 
 echo "Running setup-desktop.sh"
 
+# Check if the script is run as root
+if [[ $(id -u) -ne 0 ]]; then
+    echo "  Error: This script must be run as root. Please use 'USER root' in your Dockerfile before running this script."
+    echo "  Remember to switch back to the non-root user with 'USER ${NB_USER}' after running this script."
+    exit 1
+fi
+
+# Check if a filename argument is provided
+if [ -n "$1" ]; then
+    echo "  Warning: Passed-in file '$1' is ignored. Looking for Desktop files in the 'Desktop' directory in your repository." >&2
+fi
+
 echo "  Checking for ${REPO_DIR}/childimage/..."
 if [ -d "${REPO_DIR}/childimage/" ]; then
     cd "${REPO_DIR}/childimage/" || exit 1
 
     echo "  Checking for Desktop directory..."
     if test -d "${REPO_DIR}/childimage/Desktop"; then
-        # Check if the script is run as root
-        if [[ $(id -u) -ne 0 ]]; then
-            echo "  Error: This script must be run as root." >&2  # Output error message to standard error
-            exit 1  # Exit with a non-zero status to indicate failure
-        fi
 
-        echo "  Running setup-desktop.sh as root. Proceeding with installation..."
+        echo "  ${REPO_DIR}/childimage/Desktop directory found. Proceeding with installation..."
 
         mkdir -p "${REPO_DIR}/Desktop"
         cp -r ${REPO_DIR}/childimage/Desktop/* "${REPO_DIR}/Desktop/" 2>/dev/null
@@ -34,3 +41,5 @@ if [ -d "${REPO_DIR}/childimage/" ]; then
 else
     echo "  Directory ${REPO_DIR}/childimage/ does not exist. Skipping script."
 fi
+
+echo "  Success! install-desktop.sh"
