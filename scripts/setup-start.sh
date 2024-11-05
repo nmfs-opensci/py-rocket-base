be6a5200c8d29dc75a181ac094b99e12c81c9450
Eli Holmes - NOAA <eli.holmes@noaa.gov>
2024-11-05 08:04:51 -0800

    Update setup-start.sh

--- scripts/setup-start.sh
+++ scripts/setup-start.sh
@@ -9,6 +9,11 @@ fi
 
 echo "Running setup-start.sh as ${NB_USER}"
 
+# Check if a filename argument is provided
+if [ -n "$1" ]; then
+    echo "  Warning: Passed-in file '$1' is ignored. Looking for a file named 'start' in your repository." >&2
+fi
+
 echo "  Checking for ${REPO_DIR}/childimage/..."
 if [ -d "${REPO_DIR}/childimage/" ]; then
     cd "${REPO_DIR}/childimage/" || exit 1
@@ -16,11 +21,6 @@ if [ -d "${REPO_DIR}/childimage/" ]; then
     echo "  Checking for start in ${REPO_DIR}/childimage/..."
     if test -f "start"; then
         echo "  start found in ${REPO_DIR}/childimage/."
-        # Switch to NB_USER only if the start file exists
-        if [[ $(id -u) -eq 0 ]]; then
-            echo "  Switching to ${NB_USER} to run setup-start.sh"
-            exec su "${NB_USER}" -c "/bin/bash $0"  # Switches to NB_USER and reruns the script
-        fi
         chmod +x start
     else
         echo "  No start file found in ${REPO_DIR}/childimage/. Skipping."
@@ -28,3 +28,5 @@ if [ -d "${REPO_DIR}/childimage/" ]; then
 else
     echo "  Directory ${REPO_DIR}/childimage/ does not exist. Skipping."
 fi
+
+echo "  Success! setup-start.sh"
