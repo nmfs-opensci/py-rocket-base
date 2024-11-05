2f308ad5a3f134fb2ba46a91bfba61dc14545579
Eli Holmes - NOAA <eli.holmes@noaa.gov>
2024-11-05 07:44:10 -0800

    Update install-pip-packages.sh

--- scripts/install-pip-packages.sh
+++ scripts/install-pip-packages.sh
@@ -3,23 +3,35 @@
 
 echo "Running install-pip-packages.sh"
 
-echo "  Checking for ${REPO_DIR}/childimage/..."
-if [ -d "${REPO_DIR}/childimage/" ]; then
-    cd "${REPO_DIR}/childimage/" || exit 1
-
-    echo "  Checking for requirements.txt in ${REPO_DIR}/childimage/..."
-    if test -f "requirements.txt"; then
-        # Switch to NB_USER only if requirements.txt exists
-        if [[ $(id -u) -eq 0 ]]; then
-            echo "Switching to ${NB_USER} to run install-pip-packages.sh"
-            exec su "${NB_USER}" -c "/bin/bash $0"  # Switches to NB_USER and reruns the script
-        fi
-
-        echo "  Installing pip packages from requirements.txt as ${NB_USER}..."
-        ${NB_PYTHON_PREFIX}/bin/pip install --no-cache -r requirements.txt
-    else
-        echo "  No requirements.txt found. Skipping pip installation."
-    fi
-else
-    echo "  Directory ${REPO_DIR}/childimage/ does not exist. Skipping script."
+# Check that a file name is provided
+if [ -z "$1" ]; then
+    echo "Error: No file provided. Usage: RUN /pyrocket_scripts/install-pip-packages.sh <requirements.txt>"
+    exit 1
 fi
+
+# Set variable for the provided file
+requirements_file="$1"
+echo "Using packages file: ${requirements_file}"
+
+# Check if the specified file exists
+if [ ! -f "${requirements_file}" ]; then
+    echo "Error: File '${requirements_file}' not found. Ensure the file exists and try again."
+    exit 1
+fi
+
+# Check if the script is run as the expected user
+if [[ $(id -u) -ne $(id -u "${NB_USER}") ]]; then
+    echo "Error: This script must be run as ${NB_USER}. Please use USER ${NB_USER} in your Dockerfile before running this script."
+    exit 1
+fi
+
+echo "  Installing pip packages from ${requirements_file} as ${NB_USER}..."
+
+# Install pip packages and handle errors
+if ! ${NB_PYTHON_PREFIX}/bin/pip install --no-cache -r "${requirements_file}"; then
+    echo "Error: Installation of packages from '${requirements_file}' failed. Please check the package names and try again."
+    exit 1
+fi
+
+echo "  Pip packages installed successfully."
+
