96159ebf446bec4397b51b9b3ed46d808275f29a
Eli Holmes - NOAA <eli.holmes@noaa.gov>
2024-11-07 11:24:14 -0800

    Update install-vscode-extensions.sh

--- scripts/install-vscode-extensions.sh
+++ scripts/install-vscode-extensions.sh
@@ -3,6 +3,13 @@
 # Install VSCode extensions. 
 # These get installed to $CONDA_PREFIX/envs/notebook/share/code-server/extensions/
 
+# Check if a filename argument is provided
+if [ -z "$1" ]; then
+    echo "  Error: No file with extension names provided. Please specify a file."
+    echo "  Usage: RUN /pyrocket_scripts/install-vscode-extensions.sh <text file>"
+    exit 1
+fi
+
 ext_file="$1"
 
 echo "Checking for '$ext_file'..."
