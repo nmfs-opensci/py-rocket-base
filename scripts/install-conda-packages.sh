9f4737edb5a70fa6b7db489a942350cc9acfc20f
Eli Holmes - NOAA <eli.holmes@noaa.gov>
2024-11-17 09:06:41 -0800

    Update install-conda-packages.sh

--- scripts/install-conda-packages.sh
+++ scripts/install-conda-packages.sh
@@ -80,16 +80,13 @@ if [ "$INSTALLATION_HAPPENED" = true ]; then
     fi
 fi
 
-# Check if the required pip packages are installed because updating the conda env might delete these
-PACKAGES=("jupyter-remote-desktop-proxy" "another-package-name")
-
-for PACKAGE in "${PACKAGES[@]}"; do
-    if ! ${CONDA_DIR}/envs/$ENV_NAME/bin/pip show "$PACKAGE" > /dev/null 2>&1; then
-        echo "  Package '$PACKAGE' is missing. Reinstalling it."
-        ${CONDA_DIR}/envs/$ENV_NAME/bin/pip install --no-deps "$PACKAGE"
-    else
-        echo "  Package '$PACKAGE' is installed."
-    fi
-done
+# Check if the required pip package is installed
+PACKAGE_NAME="jupyter-remote-desktop-proxy"
+if ! ${CONDA_DIR}/envs/$ENV_NAME/bin/pip show "$PACKAGE_NAME" > /dev/null 2>&1; then
+    echo "  Package '$PACKAGE_NAME' is missing. Reinstalling it."
+    ${CONDA_DIR}/envs/$ENV_NAME/bin/pip install --no-cache "$PACKAGE_NAME"
+else
+    echo "  Package '$PACKAGE_NAME' is already installed."
+fi
 
 echo "  Success! install-conda-packages.sh"
