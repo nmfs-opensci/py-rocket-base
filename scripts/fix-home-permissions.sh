ccf1653b401c64505ebd6701706990b2075c16f1
Eli Holmes - NOAA <eli.holmes@noaa.gov>
2024-11-19 07:47:30 -0800

    Update fix-home-permissions.sh

--- scripts/fix-home-permissions.sh
+++ scripts/fix-home-permissions.sh
@@ -1,5 +1,19 @@
+#!/bin/bash
+# Required User: root
+
+echo "Running fix-home-permissions.sh"
+
+# Check if the script is run as root
+if [[ $(id -u) -ne 0 ]]; then
+    echo "Error: fix-home-permissions.sh must be run as root. Please use 'USER root' in your Dockerfile before running this script."
+    echo "Remember to switch back to the non-root user with 'USER ${NB_USER}' after running this script."
+    exit 1
+fi
+
+echo "Running fix-home-permissions.sh as $(whoami)..."
+
 # Define the target directory
-TARGET_DIR="/home/${NB_USER}/.local"
+TARGET_DIR="/home/${NB_USER}"
 
 # Change ownership only for items not owned by ${NB_USER}
 sudo find "${TARGET_DIR}" ! -user "${NB_USER}" -exec chown "${NB_USER}:${NB_USER}" {} \;
